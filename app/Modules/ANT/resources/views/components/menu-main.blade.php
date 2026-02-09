79e71a3981645902ae4eab877b46022b2a99026e
Gustavo Gomes <gustavo@spigo.net>
2026-02-09 15:31:40 -0300

    feat: Add 'Vincular Professor' link to admin menu and redirect unauthenticated users to the register page.

--- app/Modules/ANT/resources/views/components/menu-main.blade.php
+++ app/Modules/ANT/resources/views/components/menu-main.blade.php
@@ -5,4 +5,7 @@
     <x-dropdown-link :href="route('ant.admin.home')">
         Painel Admin
     </x-dropdown-link>
+    <x-dropdown-link :href="route('ant.admin.professores.create')">
+        Vincular Professor
+    </x-dropdown-link>
 @endcan

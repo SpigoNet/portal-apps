4784294df1c84acda73b5b17784862a03581be2c
Gustavo Gomes <gustavo@spigo.net>
2026-03-07 16:18:09 -0300

    fix(mundos-de-mim): altera cdn do alpine para diretivas nativas do livewire para evitar conflitos de vite

--- app/Modules/MundosDeMim/resources/views/components/layout.blade.php
+++ app/Modules/MundosDeMim/resources/views/components/layout.blade.php
@@ -13,7 +13,7 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" />
 
     <!-- Scripts -->
-    <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
+    @livewireStyles
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 
     <style>
@@ -158,5 +158,6 @@
         </div>
     </footer>
 
+    @livewireScripts
 </body>
 </html>

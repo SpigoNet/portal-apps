5b0d5dd5bb9c3e33a6feedc2720298a2bb074471
Gustavo Gomes <gustavo@spigo.net>
2026-04-21 23:10:26 -0300

    feat: implement API routes for Mithril module and add fetch script for reports

--- app/Modules/Mithril/MithrilServiceProvider.php
+++ app/Modules/Mithril/MithrilServiceProvider.php
@@ -9,6 +9,7 @@ class MithrilServiceProvider extends ServiceProvider
 {
     /**
      * Define o namespace do módulo para as views.
+     *
      * @var string
      */
     protected $namespace = 'Mithril';
@@ -17,14 +18,19 @@ class MithrilServiceProvider extends ServiceProvider
     {
         // Carrega as views do módulo com o namespace 'Mithril'
         // Uso: view('Mithril::nome_da_view')
-        $this->loadViewsFrom(__DIR__ . '/resources/views', $this->namespace);
+        $this->loadViewsFrom(__DIR__.'/resources/views', $this->namespace);
 
         // Carrega as migrações do módulo (se houverem específicas, caso contrário usam-se as globais)
         // $this->loadMigrationsFrom(__DIR__ . '/database/migrations');
 
-        // Carrega o arquivo de rotas do módulo
-        Route::middleware(['web', 'auth']) // Adicione 'auth' se o sistema exigir login
-        ->group(__DIR__ . '/routes.php');
+        // Carrega as rotas web do módulo
+        Route::middleware(['web', 'auth'])
+            ->group(__DIR__.'/routes.php');
+
+        // Carrega as rotas de API do módulo
+        Route::middleware('api')
+            ->prefix('api')
+            ->group(__DIR__.'/api.php');
     }
 
     public function register()

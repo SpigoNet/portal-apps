57ca8149d0e44ef7548a7c8f9e599a55e5ce2df8
Gustavo Gomes <gustavo@spigo.net>
2026-05-31 12:00:20 -0300

    https 4

--- app/Http/Middleware/TrustProxies.php
+++ app/Http/Middleware/TrustProxies.php
@@ -2,10 +2,12 @@
 
 namespace App\Http\Middleware;
 
-use Illuminate\Http\Middleware\TrustProxies as Middleware;
+use Closure;
 use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Illuminate\Support\Facades\URL;
 
-class TrustProxies extends Middleware
+class TrustProxies
 {
     /**
      * The trusted proxies for this application.
@@ -15,25 +17,70 @@ class TrustProxies extends Middleware
     protected $proxies = '*';
 
     /**
-     * The headers that should be used to detect proxies.
+     * Handle an incoming request.
      *
-     * @var int
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     * @return \Symfony\Component\HttpFoundation\Response
      */
-    protected $headers = Request::HEADER_X_FORWARDED_ALL;
+    public function handle(Request $request, Closure $next): Response
+    {
+        $request->setTrustedProxies($this->proxies, $this->getHeaders());
+        
+        // Force HTTPS URL generation if behind a proxy that terminates SSL
+        if ($this->shouldForceSecureUrls($request)) {
+            URL::forceScheme('https');
+        }
+
+        return $next($request);
+    }
 
     /**
-     * Get the trusted proxy header mappings.
+     * Get the headers that should be used to detect proxies.
      *
-     * @return array<string, string|null>
+     * @return int
      */
-    protected function getHeaderMappings(): array
+    protected function getHeaders(): int
     {
-        return [
-            Request::HEADER_FORWARDED => 'FORWARDED',
-            Request::HEADER_X_FORWARDED_FOR => 'X_FORWARDED_FOR',
-            Request::HEADER_X_FORWARDED_HOST => 'X_FORWARDED_HOST',
-            Request::HEADER_X_FORWARDED_PORT => 'X_FORWARDED_PORT',
-            Request::HEADER_X_FORWARDED_PROTO => 'X_FORWARDED_PROTO',
-        ];
+        // Use the combined headers equivalent to HEADER_X_FORWARDED_ALL
+        return Request::HEADER_X_FORWARDED_FOR 
+               | Request::HEADER_X_FORWARDED_HOST 
+               | Request::HEADER_X_FORWARDED_PORT 
+               | Request::HEADER_X_FORWARDED_PROTO;
+    }
+
+    /**
+     * Determine if we should force secure URLs for generation.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return bool
+     */
+    protected function shouldForceSecureUrls(Request $request): bool
+    {
+        return $request->header('x-forwarded-proto') === 'https' ||
+               $request->header('x-forwarded-ssl') === 'on';
+    }
+}
+
+        return $next($request);
+    }
+
+    /**
+     * Get the headers that should be used to detect proxies.
+     */
+    protected function getHeaders(): int
+    {
+        return Request::HEADER_X_FORWARDED_ALL ??
+               (defined('Illuminate\Http\Request::HEADER_X_FORWARDED_ALL') ?
+                Illuminate\Http\Request::HEADER_X_FORWARDED_ALL : 30);
+    }
+
+    /**
+     * Determine if we should force secure URLs for generation.
+     */
+    protected function shouldForceSecureUrls(Request $request): bool
+    {
+        return $request->header('x-forwarded-proto') === 'https' ||
+               $request->header('x-forwarded-ssl') === 'on';
     }
 }

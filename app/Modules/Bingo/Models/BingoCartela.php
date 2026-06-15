5411b25ba156a998fcc5fd4c6428ec9479d287e0
Gustavo Gomes <gustavo@spigo.net>
2026-06-15 00:35:34 -0300

    fix(Bingo): bingo só quando completa os 9 elementos da cartela

--- app/Modules/Bingo/Models/BingoCartela.php
+++ app/Modules/Bingo/Models/BingoCartela.php
@@ -41,57 +41,6 @@ class BingoCartela extends Model
 
     public function verificarBingo(): bool
     {
-        $marcacoes = $this->marcacoes ?? [];
-        $marked = [];
-
-        foreach ($marcacoes as $m) {
-            $parts = explode('-', (string) $m);
-            if (count($parts) === 2) {
-                $marked[(int) $parts[0]][(int) $parts[1]] = true;
-            }
-        }
-
-        for ($r = 0; $r < 3; $r++) {
-            $win = true;
-            for ($c = 0; $c < 3; $c++) {
-                if (! isset($marked[$r][$c])) {
-                    $win = false;
-                }
-            }
-            if ($win) {
-                return true;
-            }
-        }
-
-        for ($c = 0; $c < 3; $c++) {
-            $win = true;
-            for ($r = 0; $r < 3; $r++) {
-                if (! isset($marked[$r][$c])) {
-                    $win = false;
-                }
-            }
-            if ($win) {
-                return true;
-            }
-        }
-
-        $win = true;
-        for ($i = 0; $i < 3; $i++) {
-            if (! isset($marked[$i][$i])) {
-                $win = false;
-            }
-        }
-        if ($win) {
-            return true;
-        }
-
-        $win = true;
-        for ($i = 0; $i < 3; $i++) {
-            if (! isset($marked[$i][2 - $i])) {
-                $win = false;
-            }
-        }
-
-        return $win;
+        return $this->todasMarcadas();
     }
 }
